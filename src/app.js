ada9a9185c2fd4fdb36104d8d3096766fbb1a7a6
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-04-29 23:25:21 +0400

    Implement pagination for job listings

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import connect from "./database/mongo.js";
+import dotenv from "dotenv";
+import { getAllJobs } from "./controllers/AllJob-controller.js";
+dotenv.config();
+
+connect();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/images", express.static("./public/logos"));
+
+app.get("/jobs/:size/:page", getAllJobs);
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000.`);
+});
